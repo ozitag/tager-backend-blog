6b8abec4f896c0261210be3d35567812df5e20e6
Vital Ozierski <v.ozierski@ozitag.com>
2020-08-18 19:30:00 +0300

    fixes

--- src/Features/Guest/ListPostsByCategoryFeature.php
+++ src/Features/Guest/ListPostsByCategoryFeature.php
@@ -8,14 +8,12 @@ use OZiTAG\Tager\Backend\Blog\Repositories\PostRepository;
 use OZiTAG\Tager\Backend\Blog\Resources\Guest\GuestPostFullResource;
 use OZiTAG\Tager\Backend\Blog\Resources\Guest\GuestPostResource;
 
-class ListPostsByCategoryFeature extends BaseFeature
+class ListPostsByCategoryFeature extends Feature
 {
     private $id;
 
-    public function __construct($id, $language)
+    public function __construct($id)
     {
-        parent::__construct($language);
-        
         $this->id = $id;
     }
 
