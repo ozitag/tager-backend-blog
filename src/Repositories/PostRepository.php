dc8c31430b82b45549ffd512b19327aaa643216e
Vital Ozierski <v.ozierski@ozitag.com>
2020-11-01 18:30:22 +0300

    fix crash 'Call to undefined method ordered()'

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -23,7 +23,7 @@ class PostRepository extends EloquentRepository implements ISearchable
             return [];
         }
 
-        return $this->model->ordered()->whereIn('id', $ids)->all();
+        return $this->model->whereIn('id', $ids)->all();
     }
 
     /**
@@ -34,7 +34,7 @@ class PostRepository extends EloquentRepository implements ISearchable
      */
     public function getByLanguage($language, $offset = 0, $limit = null)
     {
-        return $this->model->ordered()->where('language', '=', $language)->skip($offset)->take($limit)->all();
+        return $this->model->where('language', '=', $language)->skip($offset)->take($limit)->all();
     }
 
     /**
@@ -59,7 +59,7 @@ class PostRepository extends EloquentRepository implements ISearchable
      */
     public function all($offset = 0, $limit = null)
     {
-        return $this->model->ordered()->skip($offset)->take($limit)->all();
+        return $this->model->skip($offset)->take($limit)->all();
     }
 
     public function getByAlias($alias, $language = null)
